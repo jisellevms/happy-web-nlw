d555c62bbe6fa1f5052c24536fe9aa05352313a1
jisellevms <jisellevms@gmail.com>
2020-10-21 13:37:38 -0300

    colocando mapa na aplicação e trocando as imagens pelas originais

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,27 +2,29 @@ import React from 'react';
 
 import '../styles/global.css';
 import '../styles/pages/landing.css';
-import logoImg from '../images/Happy.png'
+import logoImg from '../images/logo.svg';
 import { FiArrowRight } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
 
 function Landing(){
     return(
         <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
+        <img id="logo" src={logoImg} alt="Happy"/>
         <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+          <h1 id="desc">Leve felicidade para o mundo</h1>
+          <p id="desc-p">Visite orfanatos e mude o dia de muitas crianças</p>
         </main>
 
         <div className="location">
-          <strong>Rio do Sul</strong>
-          <span>Santa Catarina</span>
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
         </div>
 
-        <a href="" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
+        </Link>
       </div>
     </div>
     );
